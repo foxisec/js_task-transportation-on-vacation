ad7bb7e77b9289628e1082a352af0fc807de9663
FoxiSec <120555808+foxisec@users.noreply.github.com>
2025-02-14 17:44:40 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-<<<<<<< HEAD
+
   const DAILY_RATE = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
@@ -20,9 +20,7 @@ function calculateRentalCost(days) {
   }
 
   return totalCost;
-=======
-  // write code here
->>>>>>> b1a3d0d9a9103838b0ee5ab218f5910b0ee87c7b
+
 }
 
 module.exports = calculateRentalCost;
